31080adcb0a32b69bb3c810d4a1daa72ae9f38ae
kareem hamdy <kareemhamdy500@gmail.com>
2020-02-15 17:43:10 +0200

    add jenkinse and docker file

---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+sudo docker build --tag=udacityCapstone .
+
+# Step 2: 
+# List docker images
+sudo docker image ls
+# Step 3: 
+# Run flask app
+sudo docker run -p 80:3000 udacityCapstone
