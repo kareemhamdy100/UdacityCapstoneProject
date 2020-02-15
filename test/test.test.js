3c5aa8e895624841de98264b71d8eb43953b1e95
kareem hamdy <kareemhamdy500@gmail.com>
2020-02-15 16:17:28 +0200

    initial commit

---
+++ test/test.test.js
@@ -0,0 +1,18 @@
+const supertest = require('supertest');
+
+// const Thread = require('../models/thread');
+const app = require('../app');
+
+const api = supertest(app);
+
+
+test('test get ', async () => {
+    jest.setTimeout(100);
+     await api.get('/').expect(200)
+
+});
+
+
+afterAll(async () => {
+    await new Promise(resolve => setTimeout(() => resolve(), 50)); // avoid jest open handle error
+  });
